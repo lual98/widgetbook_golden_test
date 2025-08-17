c74baf93cc48989ecc6d5dccbe8f45832fdf62c9
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-08-16 18:11:03 -0600

    Add documentation to WidgetbookGoldenTestsProperties

--- lib/widgetbook_golden_tests_properties.dart
+++ lib/widgetbook_golden_tests_properties.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
+/// Properties for configuring widgetbook golden tests.
 class WidgetbookGoldenTestsProperties {
+  /// The theme data used to customize the appearance of widgets in the app.
   final ThemeData? theme;
+
+  /// The locale used for localization purposes.
   final Locale? locale;
+
+  /// Delegates that provide localized resources for the app.
   final Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates;
+
+  /// Supported locales that the app supports.
   final Iterable<Locale>? supportedLocales;
+
+  /// Tag used to identify tests that should be skipped during golden testing.
   final String skipTag;
+
+  /// URL of the image to display when a network request fails.
   final String errorImageUrl;
+
+  /// URL of the image to display while a network request is in progress.
   final String loadingImageUrl;
+
+  /// Name of the group of tests being run.
   final String testGroupName;
 
   const WidgetbookGoldenTestsProperties({
