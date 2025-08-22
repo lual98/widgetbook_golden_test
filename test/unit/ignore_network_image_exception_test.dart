4d3326ff25e25c7ff955ee76662d043e5deca728
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-08-22 15:36:20 -0600

    Add test of IgnoreNetworkImageException

---
+++ test/unit/ignore_network_image_exception_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart' show expect, test;
+import 'package:widgetbook_golden_test/src/ignore_network_image_exception.dart';
+
+void main() {
+  test("toString() returns custom message", () {
+    final exception = IgnoreNetworkImageException();
+    var message = exception.toString();
+    expect(message, "Ignore network image exception");
+  });
+}
