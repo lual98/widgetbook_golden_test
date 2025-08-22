647859841d2d055c389b187b198f8b3f2596c9f1
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-08-22 12:01:10 -0600

    Fix swapping of error and loading tests

---
+++ test/integration/run_widgetbook_golden_tests_customized_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart' show ThemeData;
+import 'package:widgetbook_golden_test/src/run_widgetbook_golden_tests.dart';
+import 'package:widgetbook_golden_test/src/widgetbook_golden_tests_properties.dart';
+
+import '../../widgetbook/main.directories.g.dart';
+
+void main() {
+  runWidgetbookGoldenTests(
+    nodes: directories,
+    goldenSnapshotsOutputPath: "./customized/",
+    properties: WidgetbookGoldenTestsProperties(
+      theme: ThemeData.dark(),
+      // Swap un purpose error and loading URLs for testing purposes
+      errorImageUrl: "loading-network-image",
+      loadingImageUrl: "error-network-image",
+      skipTag: "Default",
+      testGroupName: "Widgetbook golden tests with custom properties",
+    ),
+  );
+}
