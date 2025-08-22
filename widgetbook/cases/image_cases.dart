3b788a4b0fdb8dcf2d64a3b16ecd2e9b4cf3ce94
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-08-22 16:35:41 -0600

    Format files

--- widgetbook/cases/image_cases.dart
+++ widgetbook/cases/image_cases.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
-var magentaPixel = base64Decode("""iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAD0lEQVR4AQEEAPv/AP8A/wQAAf/2bp8NAAAAAElFTkSuQmCC""");
+var magentaPixel = base64Decode(
+  """iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAD0lEQVR4AQEEAPv/AP8A/wQAAf/2bp8NAAAAAElFTkSuQmCC""",
+);
 
 @widgetbook.UseCase(name: 'ResizeCover200x200', type: Image)
 Widget buildResizeImageUseCase(BuildContext context) {
