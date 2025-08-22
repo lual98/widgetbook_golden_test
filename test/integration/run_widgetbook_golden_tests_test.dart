5065da99e71f9a54bea7fb3ce425b1ef2501df08
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-08-22 10:36:56 -0600

    Add test execution with custom properties

--- test/integration/run_widgetbook_golden_tests_test.dart
+++ test/integration/run_widgetbook_golden_tests_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart' show ThemeData;
 import 'package:widgetbook_golden_test/src/run_widgetbook_golden_tests.dart';
 import 'package:widgetbook_golden_test/src/widgetbook_golden_tests_properties.dart';
 
@@ -7,5 +8,17 @@ void main() {
   runWidgetbookGoldenTests(
     nodes: directories,
     properties: WidgetbookGoldenTestsProperties(),
+  );
+
+  runWidgetbookGoldenTests(
+    nodes: directories,
+    goldenSnapshotsOutputPath: "./customized/",
+    properties: WidgetbookGoldenTestsProperties(
+      theme: ThemeData.dark(),
+      // errorImageUrl: "loading-network-image",
+      // loadingImageUrl: "error-network-image",
+      skipTag: "Default",
+      testGroupName: "Widgetbook golden tests with custom properties",
+    ),
   );
 }
