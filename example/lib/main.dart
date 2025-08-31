cb58e6b5336c2c5a05a0a076652e60dacef98959
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-08-31 12:03:21 -0600

    Remove example references in the Readme and add widgetbook pub dev badge

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:example/widgetbook.directories.g.dart';
 import 'package:widgetbook_golden_test/widgetbook_golden_test.dart';
 
-// Main example with minimum customization.
+/// Main example with minimum customization.
+/// Check out the use cases declared in the cases folder to see some common scenarios that are supported.
 void main() {
   runWidgetbookGoldenTests(
     nodes: directories,
