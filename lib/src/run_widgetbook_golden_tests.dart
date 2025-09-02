5adc01797908be18a92815a29c42f4073d56a497
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-09-02 14:09:58 -0600

    Properly use runZoned instead of setup for http mocking

--- lib/src/run_widgetbook_golden_tests.dart
+++ lib/src/run_widgetbook_golden_tests.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_golden_test/src/create_golden_test.dart';
-import 'package:widgetbook_golden_test/src/test_http_overrides.dart';
 import 'package:widgetbook_golden_test/src/widgetbook_golden_tests_properties.dart';
 
 /// Recursively generates golden tests for all [WidgetbookUseCase]s
@@ -19,18 +16,6 @@ void runWidgetbookGoldenTests({
   String goldenSnapshotsOutputPath = ".",
 }) {
   group(properties.testGroupName, () {
-    late HttpOverrides? originalHttpOverrides;
-
-    setUpAll(() {
-      // Mock the HttpOverrides to ignore network image exceptions.
-      // Sometimes runZoned did not work properly depending on the tests.
-      originalHttpOverrides = HttpOverrides.current;
-      HttpOverrides.global = createHttpOverrides(properties);
-    });
-    tearDownAll(() {
-      HttpOverrides.global = originalHttpOverrides;
-    });
-
     _traverse(nodes, goldenSnapshotsOutputPath, properties);
   });
 }
