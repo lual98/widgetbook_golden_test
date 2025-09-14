3dce75f787df059101318406acb42fd727029872
Luis Alonso Calderón Achío <lual1398@gmail.com>
2025-09-14 16:28:58 -0600

    Add tests for copy with method of Widgetbook Golden Test Properties

--- test/unit/widgetbook_golden_tests_properties_test.dart
+++ test/unit/widgetbook_golden_tests_properties_test.dart
@@ -37,4 +37,44 @@ void main() {
     expect(customProperties.loadingImageUrl, "custom-loading-image");
     expect(customProperties.testGroupName, "Custom golden tests");
   });
+
+  test(
+    "copyWith creates a copy with no changes when no parameters are passed",
+    () {
+      final original = WidgetbookGoldenTestsProperties();
+      final copied = original.copyWith();
+
+      expect(copied.theme, isNull);
+      expect(copied.locale, isNull);
+      expect(copied.localizationsDelegates, isNull);
+      expect(copied.supportedLocales, const [Locale('en', 'US')]);
+      expect(copied.skipTag, "[skip-golden]");
+      expect(copied.errorImageUrl, "error-network-image");
+      expect(copied.loadingImageUrl, "loading-network-image");
+      expect(copied.testGroupName, "Widgetbook golden tests");
+    },
+  );
+
+  test("copyWith creates a new object with updated properties", () {
+    final original = WidgetbookGoldenTestsProperties();
+    final copied = original.copyWith(
+      theme: ThemeData(primaryColor: Colors.blue),
+      locale: Locale('fr', 'FR'),
+      localizationsDelegates: const [],
+      supportedLocales: const [Locale('en', 'US'), Locale('fr', 'FR')],
+      skipTag: "[custom-skip]",
+      errorImageUrl: "custom-error-image",
+      loadingImageUrl: "custom-loading-image",
+      testGroupName: "Custom golden tests",
+    );
+
+    expect(copied.theme, isNotNull);
+    expect(copied.locale, isNotNull);
+    expect(copied.localizationsDelegates, isNotNull);
+    expect(copied.supportedLocales.length, 2);
+    expect(copied.skipTag, "[custom-skip]");
+    expect(copied.errorImageUrl, "custom-error-image");
+    expect(copied.loadingImageUrl, "custom-loading-image");
+    expect(copied.testGroupName, "Custom golden tests");
+  });
 }
