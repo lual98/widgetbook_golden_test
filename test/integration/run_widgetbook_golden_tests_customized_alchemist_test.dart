dac3913d150c67368658fbff385eb6d1eb9c1fa9
Luis Alonso Calderón Achío <lual1398@gmail.com>
2026-01-13 15:30:53 -0600

    Test with custom properties

---
+++ test/integration/run_widgetbook_golden_tests_customized_alchemist_test.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:example/cases/text_cases.dart';
+import 'package:example/l10n/app_localizations.dart';
+import 'package:example/widgetbook.directories.g.dart';
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_golden_test/src/run_widgetbook_golden_tests.dart';
+import 'package:widgetbook_golden_test/src/widgetbook_golden_tests_properties.dart';
+
+void main() async {
+  // Pre-load assets
+  var androidSvg = await File("./test/assets/android.svg").readAsBytes();
+  var sampleJpg =
+      await File("./test/assets/lorem_picsum_sample.jpg").readAsBytes();
+  runWidgetbookGoldenTestsAlchemist(
+    nodes: directories,
+    goldenSnapshotsOutputPath: "./customized/",
+    properties: WidgetbookGoldenTestsProperties(
+      theme: ThemeData.dark(),
+      // Swap un purpose error and loading URLs for testing purposes
+      errorImageUrl: "loading-network-image",
+      loadingImageUrl: "error-network-image",
+      skipTag: "[skip-other]",
+      testGroupName: "Widgetbook golden tests with custom properties",
+      locale: Locale("es"),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      addons: [
+        ViewportAddon([AndroidViewports.samsungGalaxyA50]),
+        GridAddon(),
+        AlignmentAddon(initialAlignment: Alignment.center),
+        TextScaleAddon(initialScale: 2),
+        ThemeAddon(
+          themes: [
+            WidgetbookTheme(
+              name: 'Dark',
+              data: ThemeData.dark().copyWith(
+                extensions: [MyCustomTheme.dark()],
+              ),
+            ),
+          ],
+          themeBuilder: (BuildContext context, theme, Widget child) {
+            return Theme(data: theme, child: child);
+          },
+        ),
+      ],
+      networkImageResolver: (uri) {
+        if (uri.path.toLowerCase().endsWith(".svg")) {
+          return androidSvg;
+        }
+        return sampleJpg;
+      },
+    ),
+  );
+}
