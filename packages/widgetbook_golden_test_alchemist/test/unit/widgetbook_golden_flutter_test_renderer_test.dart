ebfb9aa031daa96b484f021a1830f335f16c8e02
Luis Alonso Calderón Achío <lual1398@gmail.com>
2026-05-17 21:17:48 -0600

    feat: implement and test custom pump execution for Flutter and Alchemist renderers

---
+++ packages/widgetbook_golden_test_alchemist/test/unit/widgetbook_golden_flutter_test_renderer_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_golden_test_alchemist/src/widgetbook_golden_alchemist_renderer.dart';
+import 'package:widgetbook_golden_test_core/widgetbook_golden_test_core.dart';
+
+void main() {
+  group(WidgetbookGoldenAlchemistRenderer, () {
+    group("executes action custom pump", () {
+      bool customPumpExecuted = false;
+      final action = GoldenPlayAction(
+        name: "validate custom pump execution",
+        callback: (tester, find) async {},
+        customPump: (tester) async {
+          await tester.pump();
+          customPumpExecuted = true;
+        },
+      );
+      final properties = WidgetbookGoldenTestsProperties();
+      final useCase = WidgetbookUseCase(
+        name: "custom pump test",
+        builder: (_) => SizedBox.shrink(),
+      );
+
+      final renderer = WidgetbookGoldenAlchemistRenderer();
+      renderer.renderGoldenPlayActionTest(
+        action: action,
+        useCase: useCase,
+        goldenPath: "./snaphots/",
+        properties: properties,
+        skip: false,
+      );
+
+      tearDownAll(() {
+        expect(customPumpExecuted, true);
+      });
+    });
+  });
+}
